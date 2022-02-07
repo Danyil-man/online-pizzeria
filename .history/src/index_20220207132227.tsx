7470069357a0432a716781b78ceaf74e547004cb
Danyil <demyanyuk76@gmail.com>
2022-02-07 13:28:33 +0200

    added redux

--- .history/src/index_20220207132227.tsx
+++ .history/src/index_20220207132227.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './scss/app.scss'
-import store from './store/reduxStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <App />
     </Provider>
 
---
+++ src/store/reduxStore.ts
@@ -0,0 +1,10 @@
+import { combineReducers, createStore } from "redux";
+
+
+const reducerPack = combineReducers({
+
+})
+
+const store = createStore(reducerPack)
+
+export default store
